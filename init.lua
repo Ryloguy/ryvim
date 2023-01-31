1f126151e5b75dcfd3d89e5bd343fa0504e292f8
ryloguy <ryloguy87@gmail.com>
2023-01-30 16:33:16 -0800

    Moved init.vim to init.lua and refactored neovide support using a separate file.

---
+++ init.lua
@@ -0,0 +1,40 @@
+---------------------------------------------
+-- ██████  ██    ██ ██    ██ ██ ███    ███ --
+-- ██   ██  ██  ██  ██    ██ ██ ████  ████ --
+-- ██████    ████   ██    ██ ██ ██ ████ ██ --
+-- ██   ██    ██     ██  ██  ██ ██  ██  ██ --
+-- ██   ██    ██      ████   ██ ██      ██ --
+---------------------------------------------
+
+-- Vim Options
+vim.o.exrc = true
+vim.o.number = true
+vim.o.incsearch = true
+vim.o.scrolloff = 8
+vim.o.signcolumn = "yes"
+vim.o.relativenumber = true
+vim.o.termguicolors = true
+vim.cmd([[syntax on]])
+
+-- Maps
+vim.api.nvim_set_keymap('n', 'F8', ':TagbarToggle<CR>', {silent = true})
+
+-- VimPlug
+local Plug = vim.fn['plug#']
+
+vim.call('plug#begin', '~/.config/nvim/plugged')
+Plug 'preservim/tagbar'
+Plug 'ayu-theme/ayu-vim'
+Plug 'itchyny/lightline.vim'
+Plug 'Yggdroot/indentline'
+Plug 'pedrohdz/vim-yaml-folds'
+Plug 'lambdalisue/suda.vim'
+Plug 'williamboman/mason.nvim'
+vim.call('plug#end')
+
+-- Colorscheme
+vim.cmd([[colorscheme ayu]])
+vim.cmd([[let ayucolor="dark"]])
+
+-- Neovide Configuration
+vim.cmd('source neovide.vim')
---
+++ neovide.vim
@@ -0,0 +1,6 @@
+if exists("g:neovide")
+	let g:neovide_transparency = 0.95
+	let g:neovide_hide_mouse_when_typing = v:true
+	let g:neovide_cursor_animation_length = 0.03
+	let g:neovide_cursor_vfx_mode = "pixiedust"
+endif
